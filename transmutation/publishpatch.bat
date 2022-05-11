dab35021358cf7fcfe84b9085fc02330a3a7a739
Symphony <FLDMSymphony@gmail.com>
2022-05-11 01:04:57 -0700

    publish batch fixes
    Temp addresses pending racial subfolders

--- transmutation/publishpatch.bat
+++ transmutation/publishpatch.bat
@@ -8,30 +8,30 @@ echo.
 echo Last update to /sources directory:
 forfiles /p .. /m sources /c "cmd /c echo @file @fdate @ftime"
 echo.
-echo Ready to convert sources to DDS. Press Enter to begin.
+echo Ready to convert sources to DDS.
 pause
 echo Converting to DDS
 echo =====================
-nwn_crunch.exe -file "..\sources\*\*h.png" -outdir ".\haktemp" -fileformat dds -DXT1 -yflip -quiet
-nwn_crunch.exe -file "..\sources\*\*l.png" -outdir ".\haktemp" -fileformat dds -DXT1 -yflip -quiet
-nwn_crunch.exe -file "..\sources\*\*m.png" -outdir ".\haktemp" -fileformat dds -DXT1 -yflip -quiet
-nwn_crunch.exe -file "..\sources\*\*s.png" -outdir ".\haktemp" -fileformat dds -DXT1 -yflip -quiet
-nwn_crunch.exe -file "..\sources\*\*t.png" -outdir ".\haktemp" -fileformat dds -DXT1 -yflip -quiet
+nwn_crunch.exe -file "..\sources\female\*.png" -outdir ".\haktemp" -fileformat dds -DXT1 -yflip -quiet
+nwn_crunch.exe -file "..\sources\male\*.png" -outdir ".\haktemp" -fileformat dds -DXT1 -yflip -quiet
+nwn_crunch.exe -file "..\sources\other\*.png" -outdir ".\haktemp" -fileformat dds -DXT1 -yflip -quiet
 echo.
 echo DDS Portraits Conversion Finished
 echo.
-echo Ready to compile DDS files into patch hak. Press Enter to begin.
+echo Ready to compile DDS files into patch hak.
 pause
-nwn_erf.exe -c -f "..\Arelith Player Portrait Patch Pack\" haktemp
+nwn_erf.exe -c -f "..\Arelith Player Portrait Patch Pack\patch\appp_por.hak" haktemp
 echo.
 echo Patch Hak Creation Finished
 DEL .\haktemp\*dds
 echo Temp DDS files deleted.
 echo.
-echo Ready to convert medium portraits to TGA. Press Enter to begin.
+echo Ready to convert medium portraits to TGA.
 pause
 echo Converting to TGA
 echo =====================
-nwn_crunch.exe -file "..\sources\*\*m.png" -outdir "..\Arelith Player Portrait Patch Pack\portraits" -fileformat tga -quiet
+nwn_crunch.exe -file "..\sources\female\*m.png" -outdir "..\Arelith Player Portrait Patch Pack\portraits" -fileformat tga -quiet
+nwn_crunch.exe -file "..\sources\male\*m.png" -outdir "..\Arelith Player Portrait Patch Pack\portraits" -fileformat tga -quiet
+nwn_crunch.exe -file "..\sources\other\*m.png" -outdir "..\Arelith Player Portrait Patch Pack\portraits" -fileformat tga -quiet
 echo Conversion Finished
 pause
